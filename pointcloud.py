7012394713cc288fb2c7ced31d9fcd56ae294bca
takumi <tkmoda@gmail.com>
2024-05-14 00:07:53 +0900

    add code of down-sampling and normalizetion

--- pointcloud.py
+++ pointcloud.py
@@ -1,7 +1,10 @@
 # import laspy
 import open3d, laspy
+# import open3d.visualization
 
 path = r"C:\Users\takumi\Documents\pointcloud\ShibuyaUnderground.pts"
 
 pc = open3d.io.read_point_cloud(path)
-
+pc_downsample = pc.uniform_down_sample(every_k_points=100)
+pc_downsample.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+open3d.visualization.draw_geometries([pc,pc_downsample],point_show_normal=True)
