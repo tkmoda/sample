dc0ca06198f0ad78c9344a8757c5450a5f89a48f
takumi <tkmoda@gmail.com>
2024-06-05 22:56:00 +0900

    Colorise pointcloud by normal

--- open3d_practice.py
+++ open3d_practice.py
@@ -12,8 +12,11 @@ pc = open3d.io.read_point_cloud(inputPath)
 # open3d.visualization.draw_geometries([pc],width=900,height=600)
 pc_downsample = pc.uniform_down_sample(every_k_points=1)
 # pc_downsample = pc
-pc_downsample.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=1, max_nn=30))
+pc_downsample.estimate_normals(search_param=open3d.geometry.KDTreeSearchParamHybrid(radius=0.5, max_nn=20))
+
+pc_downsample.colors = pc_downsample.normals
+
 open3d.visualization.draw_geometries([pc_downsample],width=900,height=600)
 # open3d.visualization.draw_geometries([pc,pc_downsample],width=900,height=600,point_show_normal=True)
 
-print(pc_downsample)
+print(pc_downsample.normals)
